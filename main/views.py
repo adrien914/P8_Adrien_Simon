4160dd90a34d89d9bb30ee0b1bf08bc69d8b2d81
adrien914 <Tupuduku.91322>
2020-10-08 16:55:39 +0200

    finished index + mentions

--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html',)
+
+
+def mentions(request):
+    return render(request, 'mentions.html')
