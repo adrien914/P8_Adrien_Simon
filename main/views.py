a47c7ab52aec3c2f1d210b2164d0f136793a7d35
adrien914 <Tupuduku.91322>
2020-10-09 10:59:13 +0200

    changed view to fill db if it's not already

--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from main.models import Aliment
+from main.utils.OpenApi import OpenApi
 
 # Create your views here.
 def index(request):
+    if len(Aliment.objects.all()) < 10*20:
+        OpenApi().fill_database()
     return render(request, 'index.html',)
 
 
