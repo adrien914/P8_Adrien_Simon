328f467e433e1342e1a27d70c26025b560763aa5
adrien <adriensimon91@gmail.com>
2020-10-14 14:37:20 +0200

    image url, can't migrate cause windows is garbage

--- main/models.py
+++ main/models.py
@@ -13,6 +13,7 @@ class Aliment(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     nutrition_grades = models.CharField(max_length=1, default=None, null=True, blank=True)
     stores = models.CharField(max_length=128, default=None, null=True, blank=True)
+    image_url = models.CharField(max_length=255, default=None, null=True, blank=True)
 
     def __str__(self):
         return self.product_name
@@ -24,3 +25,4 @@ class Substitute(models.Model):
     nutrition_grades = models.CharField(max_length=1, default=None, null=True, blank=True)
     stores = models.CharField(max_length=128, default=None, null=True, blank=True)
     aliment = models.ForeignKey(Aliment, on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=255, default=None, null=True, blank=True)
