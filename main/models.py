870009d5ab714479aac98b4205394536e6fe5de8
adrien914 <Tupuduku.91322>
2020-10-30 14:55:15 +0100

    id auto

--- main/models.py
+++ main/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=128, default=None)
     url = models.CharField(max_length=128, default=None)
 
 
 class Aliment(models.Model):
-    id = models.IntegerField(primary_key=True)
     product_name = models.CharField(max_length=128, default=None, unique=True, null=True, blank=True)
     nutrition_grades = models.CharField(max_length=1, default=None, null=True, blank=True)
     stores = models.CharField(max_length=128, default=None, null=True, blank=True)
@@ -17,7 +15,6 @@ class Aliment(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class Substitute(models.Model):
-    id = models.IntegerField(primary_key=True)
     product_name = models.CharField(max_length=128, default=None, null=True, blank=True)
     nutrition_grades = models.CharField(max_length=1, default=None, null=True, blank=True)
     stores = models.CharField(max_length=128, default=None, null=True, blank=True)
